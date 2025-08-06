ae5238bd112c5400365cbfccd925ac7d6cb6b184
Emir Ale <43660559+emiir14@users.noreply.github.com>
2025-08-06 01:45:29 -0300

    Update vite.config.ts

--- shadcn-ui/vite.config.ts
+++ shadcn-ui/vite.config.ts
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/lawFirm_template/', // 👈 this is needed for GitHub Pages
+  base: '/', 
 })
+
 
